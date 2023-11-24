99ebfb40a80aeb06d2ad31ca7b333020c8edd707
mp-balaji <mpbalaji206@gmail.com>
2023-11-24 21:12:26 +0000

    Added code for sqlite db creation

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+    
     # . refers to current directory
     # .main refers to the file named "main" in the current directory
     from .main import main as main_blueprint
