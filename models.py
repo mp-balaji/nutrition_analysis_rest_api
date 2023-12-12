2784e73df8d932913c722ebb6675850be601d3fd
Balaji M P <104824947+mp-balaji@users.noreply.github.com>
2023-12-12 21:31:20 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
-from . import db
+from extensions import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
