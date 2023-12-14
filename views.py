1260e54ad8e52aae0355285b3bd52341a29fed5c
Balaji M P <104824947+mp-balaji@users.noreply.github.com>
2023-12-14 18:58:40 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 # views.py
-from flask import render_template, request, redirect, url_for, session, Blueprint, flash
+from flask import render_template, request, redirect, url_for, session, Blueprint, flash, jsonify
 from models import User
 from extensions import db, bcrypt
 from flask_bcrypt import Bcrypt
@@ -23,13 +23,13 @@ def signup():
         ):
             # User already exists
             flash("Username or email already in use.", "error")
-            return redirect(url_for("views.signup"))
+            return redirect(url_for("views.signup")), 400
 
         new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
         flash("Account created successfully, please login.", "success")
-        return redirect(url_for("views.login"))
+        return redirect(url_for("views.login")), 201
 
     return render_template("signup.html", title="Sign Up")
 
@@ -44,10 +44,10 @@ def login():
         if user and bcrypt.check_password_hash(user.password, password):
             session["username"] = user.username
             flash("Logged in successfully.", "success")
-            return redirect(url_for("views.home"))
+            return redirect(url_for("views.home")), 200
         else:
             flash("Invalid email or password.", "error")
-            return redirect(url_for("views.home"))
+            return redirect(url_for("views.home")) , 401
 
     return render_template("login.html", title="Login")
 
@@ -56,16 +56,16 @@ def login():
 def logout():
     session.pop("username", None)
     flash("You have been logged out.", "info")
-    return redirect(url_for("views.login"))
+    return redirect(url_for("views.login")) , 200
 
 @views.route("/")
 def home():
     if "username" in session:
         return render_template("home.html", title="Home")
     flash("Please login to view this page.", "info")
-    return redirect(url_for("views.login"))
+    return redirect(url_for("views.login")), 200
 
-@views.route("/delete/<user>", methods=['GET', 'POST'])
+@views.route("/delete/<user>", methods=['DELETE'])
 def delete(user):
     data = User.query.filter_by(username=user).first()
     if data:
@@ -73,17 +73,19 @@ def delete(user):
         db.session.commit()
         session.pop("username", None)
         print('Your account has been deleted!')
-        return redirect(url_for('views.login'))
+        return jsonify({'success': True})
+        #return redirect(url_for('views.login')), 200
     else:
-        return redirect(url_for('views.signup'))
         print("Not working")
+        return redirect(url_for('views.signup'))
+        
 
-@views.route("/update/<user>", methods=['GET', 'POST'])
+@views.route("/update/<user>", methods=['GET', 'PATCH'])
 def update(user):
     if request.method == 'GET':
         return render_template("update.html", title="Update")
     else:
-        new_password = request.form.get('new_password')
+        new_password = request.json.get('new_password')
         data = User.query.filter_by(username=user).first()
 
         if data:
@@ -94,7 +96,7 @@ def update(user):
             db.session.commit()
 
             print('Your password has been updated!')
-            return redirect(url_for('views.login'))
+            return redirect(url_for("views.login")), 200
         else:
             print('User not found!', 'error')
-            return redirect(url_for('views.login'))
+            return redirect(url_for("views.home")), 404
